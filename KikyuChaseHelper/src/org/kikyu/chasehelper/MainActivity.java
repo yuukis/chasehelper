64281668e0618541360f4d11d45ecfab5bfe85b7
Yuuki SHIMIZU <yuuki@maxio.jp>
2013-06-11 01:06:29 +0900

    地図を表示 closes #1

--- KikyuChaseHelper/src/org/kikyu/chasehelper/MainActivity.java
+++ KikyuChaseHelper/src/org/kikyu/chasehelper/MainActivity.java
@@ -1,7 +1,7 @@
 package org.kikyu.chasehelper;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
